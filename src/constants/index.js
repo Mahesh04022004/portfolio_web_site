6f710c15b1bb1df130f25b70365f402b40269a71
MAHESH04022004 <maheshmishra1069@gmail.com>
2024-11-19 11:33:19 +0530

    Initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,134 @@
+
+import {
+    car,
+    contact,
+    css,
+    estate,
+    express,
+    git,
+    github,
+    html,
+    javascript,
+    linkedin,
+    mongodb,
+    motion,
+    mui,
+    nextjs,
+    nodejs,
+    pricewise,
+    react,
+    redux,
+    sass,
+    snapgram,
+    summiz,
+    tailwindcss,
+    threads,
+    typescript,
+} from "../assets/icons";
+
+
+export const skills = [
+    
+    {
+        imageUrl: typescript,
+        name: "Java",
+        type: "Backend",
+    }
+    ,{
+        imageUrl: sass,
+        name: "Spring Boot",
+        type: "Backend",
+    },
+    {
+        imageUrl: css,
+        name: "CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: git,
+        name: "Git",
+        type: "Version Control",
+    },
+    {
+        imageUrl: github,
+        name: "GitHub",
+        type: "Version Control",
+    },
+    {
+        imageUrl: html,
+        name: "HTML",
+        type: "Frontend",
+    },
+    {
+        imageUrl: javascript,
+        name: "JavaScript",
+        type: "Frontend",
+    },
+    {
+        imageUrl: motion,
+        name: "Motion",
+        type: "Animation",
+    },
+    {
+        imageUrl: react,
+        name: "React",
+        type: "Frontend",
+    },
+    {
+        imageUrl: redux,
+        name: "Redux",
+        type: "State Management",
+    },
+
+    {
+        imageUrl: tailwindcss,
+        name: "Tailwind CSS",
+        type: "Frontend",
+    },
+    
+];
+
+
+
+export const socialLinks = [
+    {
+        name: 'Contact',
+        iconUrl: contact,
+        link: '/contact',
+    },
+    {
+        name: 'GitHub',
+        iconUrl: github,
+        link: 'https://github.com/MAHESH04022004',
+    },
+    {
+        name: 'LinkedIn',
+        iconUrl: linkedin,
+        link: 'https://www.linkedin.com/in/mahesh-mishra-a8b888276/',
+    }
+];
+
+export const projects = [
+    {
+        iconUrl: pricewise,
+        theme: 'btn-back-red',
+        name: 'React prtoject Implementation',
+        description: 'Developed a web application showcasing my skill on react it is not about the UI/UX this project just allows me to actually build something using react.',
+        link: 'https://github.com/Mahesh04022004/React_project_2.git',
+    },
+    {
+        iconUrl: threads,
+        theme: 'btn-back-green',
+        name: 'Socail Media API',
+        description: 'Created an API using Spring Boot which is similar to like of a social media,It has some basic feature which can be tested using Postman.',
+        link: 'https://github.com/Mahesh04022004/social-media-rest-api-basic.git',
+    },
+    {
+        iconUrl: car,
+        theme: 'btn-back-blue',
+        name: 'ENotes',
+        description: 'Designed and built an webapp which is used take notes online, this project was developed using advance Java concept like Servlet,Jsp,Hibernate.',
+        link: 'https://github.com/adrianhajdin/project_next13_car_showcase',
+    },
+    
+];
---
+++ src/pages/About.jsx
@@ -0,0 +1,61 @@
+
+
+import { CTA } from "../components";
+import { skills } from "../constants";
+
+import "react-vertical-timeline-component/style.min.css";
+
+const About = () => {
+  return (
+    <section className='max-container'>
+      <h1 className='head-text'>
+        Hello, I'm{" "}
+        <span className='blue-gradient_text font-semibold drop-shadow'>
+          {" "}
+          Mahesh
+        </span>{" "}
+        👋
+      </h1>
+
+      <div className='mt-5 flex flex-col gap-3 text-slate-500'>
+        <p>
+          Software Engineer from india, work on both frontend and backend.
+        </p>
+      </div>
+
+      <div className='py-10 flex flex-col'>
+        <h3 className='subhead-text'>My Skills</h3>
+
+        <div className='mt-16 flex flex-wrap gap-12'>
+          {skills.map((skill) => (
+            <div className='block-container w-20 h-20' key={skill.name}>
+              <div className='btn-back rounded-xl' />
+              <div className='btn-front rounded-xl flex justify-center items-center'>
+                <img
+                  src={skill.imageUrl}
+                  alt={skill.name}
+                  className='w-1/2 h-1/2 object-contain'
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className='py-16'>
+        <h3 className='subhead-text'>Work Experience.</h3>
+        <div className='mt-5 flex flex-col gap-3 text-slate-500'>
+          <p>
+             I am still B-Teach Student who is ready to work and interested in learning some experience in my carrer
+          </p>
+        </div>
+      </div>
+
+      <hr className='border-slate-200' />
+
+      <CTA />
+    </section>
+  );
+};
+
+export default About;
